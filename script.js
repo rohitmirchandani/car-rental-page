c521bc964b4266d4e18fee0796e196d3060ceae9
rohitmirchandani <rohitmirchandani365@gmail.com>
2022-09-02 00:20:05 +0530

    m

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let header = document.getElementById("header")
+let background = document.getElementById("main-background")
+let bgflag = true;
 window.addEventListener("scroll",()=>{
     if(window.scrollY === 0){
         header.classList.remove("not-top");
@@ -6,3 +8,17 @@ window.addEventListener("scroll",()=>{
         header.classList.add("not-top");
     }
 })
+
+function changebg(){
+    setTimeout(()=>{
+        if(bgflag){
+            background.src = "background-image2.jpg"
+        }else{
+            background.src = "background-image.jpg"
+        }
+        bgflag = !bgflag;
+        changebg();
+    }, 8000)
+}
+changebg();
+
